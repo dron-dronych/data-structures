c2f6a18a6b441bfed39ef0bc9ca45923714387f2
dron <dr_dronych@mail.ru>
2022-07-07 02:43:25 +0600

    added node representation

--- tree_height.py
+++ tree_height.py
@@ -6,15 +6,25 @@ class Tree:
         """
         assert len(nodes) == n, f'`nodes` should contain exactly {n} elements'
 
+        nodes_list = []
+
         for i in range(n):
             node = Node(i)
             parent = nodes[i]
 
             if parent == -1:
                 self.root = node
+                nodes_list.append(node)
                 continue
 
             node.parent = Node(parent)
+
+            nodes_list.append(node)
+
+        self.nodes = nodes_list
+
+    def height(self):
+        pass
 
 
 class Node:
@@ -22,10 +32,16 @@ class Node:
         self.key = key
         self.parent = None
 
+    def __repr__(self):
+        return f'Node(key={self.key}, parent={self.parent})'
 
-n = 4
+
+
+
+
+n = 5
 nodes = [4, -1, 4, 1, 1]
 
 tree = Tree(n, nodes)
-print(tree.root.key)
+print(tree.nodes)
 
