08af79ae9ae3bcb3fee43c6bc127c8ca66de281f
dron <dr_dronych@mail.ru>
2022-10-02 11:20:30 +0600

    added test on substring equality

---
+++ tests/test_susbstring_equality.py
@@ -0,0 +1,15 @@
+import pytest
+from substring_equality import substring_equality
+
+
+expected = [
+    (('trololo', 2, 4, 3), 1),
+    (('trololo', 3, 5, 1), 1),
+    (('trololo', 0, 0, 7), 1),
+    (('trololo', 1, 3, 2), 0),
+]
+
+
+@pytest.mark.parametrize('test_input, expected', expected)
+def test_substring_equality(test_input, expected):
+    assert substring_equality(*test_input) == expected
